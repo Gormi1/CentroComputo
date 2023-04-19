9f025f61badb42931d7681cfad1c96efc43201ef
310318AAC <126709945+310318AAC@users.noreply.github.com>
2023-04-19 13:33:36 -0700

    Actualizacion de la migracion de la tabla bitacora no repetida y agregamos el formato para registro de la matricula

--- app/Http/Controllers/registroBitacoraController.php
+++ app/Http/Controllers/registroBitacoraController.php
@@ -18,29 +18,36 @@ class registroBitacoraController extends Controller
 
     public function store(Request $request)
     {
+        try {
+            $validatedData = $request->validate([
+                'Matricula' => 'required',
+                'Usuario' => 'required',
+            ]);
 
+            // Si los datos son validados, continúa con el procesamiento de los datos
+            $registro = new bitacora;
+            $matricula = $validatedData['Matricula'];
+            $registro->Matricula =  $matricula;
+            $registro->Usuario = $validatedData['Usuario'];
+            // Comprobamos si el valor del campo de acceso tiene la estructura "xx-xx-xxxx" y solo contiene números
+            if (preg_match('/^\d{2}-\d{2}-\d{4}$/', $matricula)) {
+                    // Si tiene la estructura correcta y solo contiene números, podemos permitir el acceso
+                    // además de guardar los datos dentro de la bases de datos
+                    $registro->save();
+                    // y redirige al usuario a la página anterior
+                    return redirect('/')->with('success', 'Tu equpo ha sido apartado y seleccionado vaya a su lugar');
 
-    try {
-        $validatedData = $request->validate([
-            'Matricula' => 'required',
-            'Usuario' => 'required',
-        ]);
+                } else {
+                    // Si no tiene la estructura correcta o contiene caracteres que no son numéricos, mostramos un mensaje de error
+                    $msg = 'ingrese la matricuala en este formato (00-00-0000)';
+                    $error = 'error';
+                    return back()->with($error, $msg);
+                }
 
-        // Si los datos son validados, continúa con el procesamiento de los datos
-        $registro = new bitacora;
-        $registro->Matricula = $validatedData['Matricula'];
-        $registro->Usuario = $validatedData['Usuario'];
-        $registro->save();
-
-        // redirige al usuario a la página anterior
-        return redirect('/')->with('success', 'Tu equpo ha sido apartado y seleccionado valla a su lugar');
-    } catch (\Illuminate\Validation\ValidationException $e) {
-        // Mostrar una alerta al usuario indicando que se requieren los campos
-        return back()->with('error', 'Los campos Matricula y Usuario son requeridos.');
-    }
-
-
-
-
+        // return redirect('/')->with('success', 'Tu equpo ha sido apartado y seleccionado valla a su lugar');
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Mostrar una alerta al usuario indicando que se requieren los campos
+            return back()->with('error', 'Los campos Matricula y Usuario son requeridos.');
+        }
     }
 }
---
+++ app/Models/eComputo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class eComputo extends Model
+{
+    use HasFactory;
+    protected $table = 'computo';
+
+    //Defino las variables de la tabla de la base de datos
+        protected $fillable = [
+            'id',
+            'nombreEquipo',
+            'estado',
+            'ip',
+
+
+
+        ];
+
+}
